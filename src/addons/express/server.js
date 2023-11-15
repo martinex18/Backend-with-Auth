9299512cf6b4d7df8f611cbaa8cc47aefd90d1a1
Gabriel Martínez <martinegabriel47@gmail.com>
2023-11-15 00:11:27 -0500

    agg update

--- src/addons/express/server.js
+++ src/addons/express/server.js
@@ -3,14 +3,15 @@ const server = express()
 import morgan from 'morgan'
 import router_auth from '../../routes/auth.routes/router_auth.js'
 import cookieParser from 'cookie-parser'
+import cors from 'cors'
 
 
 
-
+server.use(cors('*'))
 server.use(morgan('dev'))
 server.use(express.json())
 server.use(cookieParser())
-server.use(router_auth)
+server.use('/api',router_auth)
 
 
 
--- src/database/conexion.js
+++ src/database/conexion.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const conexion = async () => {
     try{
-        await mongoose.connect(process.env.DB)
+        await mongoose.connect('mongodb+srv://Gabriel:Gabrielmbravo1805@cluster0.ufdnstb.mongodb.net/?retryWrites=true&w=majority')
         console.log('Database Connected')
     }catch(error){
         console.error(error)
